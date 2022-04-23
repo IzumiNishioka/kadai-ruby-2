e3f3f9d2f5b65aebf000390c39f4c2770e84b202
izumi.nishioka <izumi.n.mail@gmail.com>
2022-04-23 09:35:34 +0000

    add method

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,20 @@
-num_max = 100
-num = 1
-
-while num <= num_max
-  if num % 3 == 0 && num % 5 == 0
-  puts "FizzBuzz"
-  elsif num % 3 == 0
-    puts "Fizz"
-  elsif num % 5 == 0
-    puts "Buzz"
-  else
-    puts num
-  end
+def fizzbuzz
+  num_max = 100
+  num = 1
   
-  num += 1
+  while num <= num_max
+    if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+    
+    num += 1
+  end
 end
+
+fizzbuzz()
