75684b8fb73995b9430a50d03ad7ffd7606f4992
izumi.nishioka <izumi.n.mail@gmail.com>
2022-04-23 10:03:00 +0000

    third commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,20 +1,18 @@
-def fizzbuzz
-  num_max = 100
-  num = 1
-  
-  while num <= num_max
-    if num % 3 == 0 && num % 5 == 0
-    puts "FizzBuzz"
-    elsif num % 3 == 0
-      puts "Fizz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    else
-      puts num
-    end
-    
-    num += 1
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return num
   end
 end
 
-fizzbuzz()
+num_max = 100
+num = 1
+while num <= num_max
+  puts fizzbuzz(num)
+  num += 1
+end
